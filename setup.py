f8300f0bb5542305677f929db5dfdc6cb3e8e07b
James Polley <jp@jamezpolley.com>
2015-04-16 12:11:05 +1000

    first test, no deps

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='foo',
+      version='0.1',
+      description='foo'
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['foo'],
+      zip_safe=False,
+  )
